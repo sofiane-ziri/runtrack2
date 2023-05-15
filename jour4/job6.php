fbcc6a1e1b8a09526c33bf90d8235222f4f0e6f8
Your Name <sofiane.ziri@laplateforme.io>
2023-05-15 13:57:18 +0200

    jour4

---
+++ jour4/job6.php
@@ -0,0 +1,19 @@
+<form method="GET" action="">
+  <label for="nombre">Entrez un nombre :</label>
+  <input type="text" id="nombre" name="nombre">
+  <button type="submit">Valider</button>
+</form>
+<?php
+if (isset($_GET['nombre'])) {
+  $nombre = $_GET['nombre'];
+  if (is_numeric($nombre)) {
+    if ($nombre % 2 == 0) {
+      echo "Nombre pair";
+    } else {
+      echo "Nombre impair";
+    }
+  } else {
+    echo "Veuillez entrer un nombre valide";
+  }
+}
+?>
